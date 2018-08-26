5f79c08bf689e74e67c1a64bf32d9f1fa672984f
Jasper Alt <jasperalt@Thoth-3.local>
2018-08-26 15:42:35 -0700

    changed scan fn to simulate torus through multiple offset tree queries rather than inserts. small time savings if any but much less memory

--- sim.py
+++ sim.py
@@ -3,12 +3,13 @@ from random import seed, random
 from operator import add
 from sys import stdout
 from scipy.stats import norm
-from scipy.spatial import distance, KDTree
+from scipy.spatial import distance
 from numpy import asarray, empty, zeros, expand_dims, cumsum, tile, copy
 from networkx import from_numpy_matrix, draw
 from pylab import figure, pause, plot, xlim, ylim, show, close
 from rtree import index
 from bintrees import FastAVLTree
+import multiprocessing as mp
 
 ###############################################################################
 
@@ -207,19 +208,7 @@ def scan(result, d, self_edges=False, weight=False, bound=None, handling=None, m
             for coord in range(D):
                 I.append(result[i][coord][t])
 
-            if bound is "Torus" and I[0] < 999999999.0:
-                # each offset (includes the non-offset)
-                for o in offsets:
-                    # add the offset
-                    m = list(map(add, I, o))
-                    # rtree coords are a tuple: (xmin, ymin, ... xmax, ymax)
-                    # representing a bounding box
-                    # for points, obviously, all min = max
-                    m += m
-                    tup = tuple(m)
-                    print(tup)
-                    idx.insert(i, tup)
-            elif I[0] < 999999999.0:
+            if I[0] < 999999999.0:
                 idx.insert(i, tuple(I + I)) # insert bounding box where min = max
 
         # now query the rtree with a d x d bounding box centered on each point
@@ -232,10 +221,23 @@ def scan(result, d, self_edges=False, weight=False, bound=None, handling=None, m
                 if I[coord] is 999999999.0:
                     skip = True
             if skip is not True:
-                minima = [x - d for x in I]
-                maxima = [x + d for x in I]
-                tup = tuple(minima + maxima)
-                hits = set(idx.intersection(tup))
+                hits = set([])
+                if handling is "Torus":
+                    for o in offsets:
+                        # add the offset
+                        m = list(map(add, I, o))
+                        # rtree coords are a tuple: (xmin, ymin, ... xmax, ymax)
+                        # representing a bounding box
+                        # for points, obviously, all min = max
+                        minima = [x - d for x in m]
+                        maxima = [x + d for x in m]
+                        tup = tuple(minima + maxima)
+                        hits = hits.union(set(idx.intersection(tup)))
+                else:
+                    minima = [x - d for x in I]
+                    maxima = [x + d for x in I]
+                    tup = tuple(minima + maxima)
+                    hits = set(idx.intersection(tup))
                 # and check whether each point within the box is within d
                 for j in hits:
                     J = []
@@ -256,10 +258,112 @@ def scan(result, d, self_edges=False, weight=False, bound=None, handling=None, m
     print("")
     return S
 
-# for the 1D case, use an AVL tree
-def scan_1D(result, d, self_edges=False, weight=False, bound=None, handling=None, memory=False):
+# scan: sim result -> list of np array adjacency matrices
+# self_edges toggles whether nodes are within distance of themselves
+# if weights, A[ij] = d(ij), else A[ij] = (d(ij) < d)
+# memory sets whether the network remembers previous edges
+# [ should probably be broken up into more functions ]
+def scan_multi(result, d, self_edges=False, weight=False, bound=None, handling=None, memory=False):
     stdout.write("Assembling graphs")
     stdout.flush()
+    # recover sim parameters
+    N = len(result[0][0]) # "N" = N + 1
+    D = len(result[0])
+    P = len(result)
+
+    if(D is 1):
+        return scan_1D(result, d, self_edges=self_edges, weight=weight, bound=bound, handling=handling, memory=memory)
+
+    p = index.Property(dimension=D)
+    # using an R*-tree variant did not produced faster speeds for any n tested
+    # (up to 10,000)
+
+    S = []          # List of adjacency matrices
+    if handling is "Torus":
+        offsets = []
+        # simulate the toroid by inserting extra points in the 'next box over'
+        # in each direction.
+        # we do this using the binary integers up to 2^D:
+        for k in range(pow(2, D)):
+            plus = [int(x) * bound for x in bin(k)[2:].zfill(D)]
+            # eg, 1 -> 0001 -> 0,0,0,1 -> 0,0,0,5 if boundary is 5
+            # then 2 -> ... -> 0,0,5,0 etc
+            for l in range(pow(2,D)):
+                minus = [-int(x) * bound for x in bin(l)[2:].zfill(D)]
+                # eg, 1 -> 0001 -> 0,0,0,-5 if boundary is 5
+                # add the positive and negative offsets...
+                m = list(map(add, plus, minus))
+                # the unique results are then the offsets in all directions
+                if m not in offsets:
+                    offsets.append(m)
+
+    # Each time
+    for t in range(N):
+        # copy previous matrix or start fresh as appropriate
+        if t is 0 or memory is False:
+            S.append(zeros((P, P)))
+        else:
+            S.append(copy(S[t-1]))
+
+        # build rtree of points unless they are out of bounds
+        idx = index.Index(properties=p)
+        for i in range(P):
+            I = []
+            # get point coordinates
+            for coord in range(D):
+                I.append(result[i][coord][t])
+
+            if I[0] < 999999999.0:
+                idx.insert(i, tuple(I + I)) # insert bounding box where min = max
+
+        # now query the rtree with a d x d bounding box centered on each point
+        # (unless the point is out of bounds)
+        for i in range(P):
+            I = []
+            skip = False
+            for coord in range(D):
+                I.append(result[i][coord][t])
+                if I[coord] is 999999999.0:
+                    skip = True
+            if skip is not True:
+                hits = set([])
+                if handling is "Torus":
+                    for o in offsets:
+                        # add the offset
+                        m = list(map(add, I, o))
+                        # rtree coords are a tuple: (xmin, ymin, ... xmax, ymax)
+                        # representing a bounding box
+                        # for points, obviously, all min = max
+                        minima = [x - d for x in m]
+                        maxima = [x + d for x in m]
+                        tup = tuple(minima + maxima)
+                        hits = hits.union(set(idx.intersection(tup)))
+                else:
+                    minima = [x - d for x in I]
+                    maxima = [x + d for x in I]
+                    tup = tuple(minima + maxima)
+                    hits = set(idx.intersection(tup))
+                # and check whether each point within the box is within d
+                for j in hits:
+                    J = []
+                    for coord in range(D):
+                        J.append(result[j][coord][t])
+                    if handling is "Torus":
+                        dist = torus_distance(I,J, bound)
+                    else:
+                        dist = distance.euclidean(I,J)
+                    # connect nodes if in distance and appropriate re self edges
+                    if dist < d:
+                        S[t][i,j] = (self_edges or i != j)
+                    if weight:
+                        S[t][i,j] *= dist
+
+        stdout.write(".")
+        stdout.flush()
+    print("")
+    return S
+# for the 1D case, use an AVL tree
+def scan_1D(result, d, self_edges=False, weight=False, bound=None, handling=None, memory=False):
     # recover sim parameters
     N = len(result[0][0]) # "N" = N + 1
     D = len(result[0])
@@ -296,9 +400,7 @@ def scan_1D(result, d, self_edges=False, weight=False, bound=None, handling=None
                     hits.append([v for (k,v) in idx.item_slice(minimum % bound, bound)])
                 for j in hits:
                     S[t][i,j] = (self_edges or i != j)
-
                     # add something to handle weight case here
-
         stdout.write(".")
         stdout.flush()
     print("")
